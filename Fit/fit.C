474a66a2bfed25ac7a14b95d1664de7d381f696c
Matt Wright <102613644+matt-wright2001@users.noreply.github.com>
2023-04-13 16:12:21 +0100

    working code generating histograms

--- Fit/fit.C
+++ Fit/fit.C
@@ -16,18 +16,31 @@ void fit(){
   int channel[maxhits];
   int eventHitCount;
 
-  TFile *hfile = hfile = TFile::Open("coincidence.root");
+  TFile *hfile = hfile = TFile::Open("False_Coincidence.root");
 
   TTree *tree = (TTree*)hfile->Get("data");
-
-  
 
   tree->SetBranchAddress("adcEnergy", adcEnergy);
   tree->SetBranchAddress("hitTime", hitTime);
   tree->SetBranchAddress("channel", channel);
   tree->SetBranchAddress("eventHitCount", &eventHitCount);
 
-  TFile *f = new TFile("histogram.root");
+  TFile *f = new TFile("histogram.root", "RECREATE");
 
-  TH1F *adcEnergy = new TH1F("adc",,)
+  TH1F *Eadc = new TH1F("Eadc", "adcEnergy vs. counts", 700, 0, 7000);
+  TH1F *hTime = new TH1F("hTime", "hitTime vs. counts", 3500000000, 0, 350000000000);
+  TH1F *chan = new TH1F("chan", "channel vs. counts", 4, 0, 4);
+  TH1F *eventhitCt = new TH1F("eventhitCt", "eventhitCount vs. counts", 2, 0, 2);
+
+  for (int i =0; i<maxhits; i++){
+    Eadc->Fill(adcEnergy[i]);
+    hTime->Fill(hitTime[i]);
+    chan->Fill(channel[i]);
+  }
+  eventhitCt->Fill(eventHitCount);
+
+  Eadc->Write();
+  hTime->Write();
+  chan->Write();
+  eventhitCt->Write();
 }
